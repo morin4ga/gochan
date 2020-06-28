85695084f4b6cc092db7631f8beca4c7e0153037
anon <29348502+anon774@users.noreply.github.com>
2020-06-29 06:59:45 +0900

    refactor(config): use expanduser()

--- gochan/config.py
+++ gochan/config.py
@@ -8,9 +8,9 @@ from asciimatics.screen import Screen
 from gochan.key import Key, parse_key
 from gochan.widgets import Brush
 
-KEYBINDINGS_PATH = Path(Path.home() / ".config/gochan/keybindings.json")
+KEYBINDINGS_PATH = Path("~/.config/gochan/keybindings.json").expanduser()
 CACHE_PATH = Path("~/.config/gochan/cache").expanduser()
-NG_PATH = Path(Path.home() / ".config/gochan/ng.json")
+NG_PATH = Path("~/.config/gochan/ng.json").expanduser()
 THEME_PATH = Path("~/.config/gochan/theme.json").expanduser()
 
 USE_IMAGE_CACHE = True
@@ -24,7 +24,7 @@ BROWSER_PATH = None
 USER_AGENT = "Mozilla/5.0"
 COOKIE = "yuki=akari"
 
-conf_file = Path(Path.home() / ".config/gochan/conf.json")
+conf_file = Path("~/.config/gochan/conf.json").expanduser()
 
 if conf_file.is_file():
     conf = json.loads(conf_file.read_text())
