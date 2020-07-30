790b95d571a67da8270c9146e07c42deafac3a6d
anon <29348502+anon774@users.noreply.github.com>
2020-07-30 21:01:52 +0900

    feat(view_models): add FavoriteVM

--- gochan/view_models/favoritesvm.py
+++ gochan/view_models/favoritesvm.py
@@ -1,12 +1,32 @@
+from typing import Union
 from gochan.event_handler import PropertyChangedEventHandler, PropertyChangedEventArgs
 from gochan.models import AppContext
-from gochan.models.favorites import Favorites
+from gochan.models.favorites import FavoriteThread, FavoriteBoard
 
 
 class FavoritesVM:
     def __init__(self, app_context: AppContext):
         super().__init__()
         self._app_context = app_context
-        s
+        self._app_context.favorites.on_property_changed.add(self._context_changed)
 
+        self.on_property_changed = PropertyChangedEventHandler()
 
+    @property
+    def list(self):
+        return self._app_context.favorites.list
+
+    def add(self, item: Union[FavoriteThread, FavoriteBoard]):
+        self._app_context.favorites.add(item)
+
+    def remove(self, item: Union[FavoriteThread, FavoriteBoard]):
+        self._app_context.favorites.remove(item)
+
+    def raise_order(self, item: Union[FavoriteThread, FavoriteBoard]):
+        self._app_context.favorites.raise_order(item)
+
+    def lower_order(self, item: Union[FavoriteThread, FavoriteBoard]):
+        self._app_context.favorites.lower_order(item)
+
+    def _context_changed(self, e: PropertyChangedEventArgs):
+        self.on_property_changed.invoke(PropertyChangedEventArgs(self, "list"))
