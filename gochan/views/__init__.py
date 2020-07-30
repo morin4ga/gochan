e53dce3aabc8c52cc849d33076760fe4c0e6ac23
anon <29348502+anon774@users.noreply.github.com>
2020-07-30 22:30:15 +0900

    feat(views): add FavoritesView

--- gochan/views/__init__.py
+++ gochan/views/__init__.py
@@ -1,5 +1,6 @@
-from .bbsmenu_view import BbsmenuView  # noqa: F401
-from .board_view import BoardView      # noqa: F401
-from .image_view import ImageView      # noqa: F401
-from .thread_view import ThreadView    # noqa: F401
-from .ng_view import NGView            # noqa: F401
+from .bbsmenu_view import BbsmenuView      # noqa: F401
+from .board_view import BoardView          # noqa: F401
+from .image_view import ImageView          # noqa: F401
+from .thread_view import ThreadView        # noqa: F401
+from .ng_view import NGView                # noqa: F401
+from .favorites_view import FavoritesView  # noqa: F401
---
+++ gochan/views/favorites_view.py
@@ -0,0 +1,73 @@
+from asciimatics.widgets import Frame, ListBox, Widget, Layout
+from asciimatics.screen import Screen
+from asciimatics.exceptions import NextScene
+from asciimatics.event import KeyboardEvent
+
+from gochan.event_handler import PropertyChangedEventArgs
+from gochan.view_models.favoritesvm import FavoritesVM, FavoriteThread
+
+
+class FavoritesView(Frame):
+    def __init__(self, screen: Screen, context: FavoritesVM):
+        super().__init__(screen,
+                         screen.height,
+                         screen.width,
+                         has_border=False,
+                         hover_focus=True,
+                         can_scroll=False,
+                         )
+
+        self._context = context
+        self._context.on_property_changed.add(self._context_changed)
+
+        self._selected_item = None
+        self._list = []
+        self._list_box = ListBox(Widget.FILL_FRAME, [], on_select=self._on_select, on_change=self._on_changed)
+
+        layout = Layout([100], fill_frame=True)
+        self.add_layout(layout)
+        layout.add_widget(self._list_box)
+
+    def process_event(self, event):
+        if isinstance(event, KeyboardEvent):
+            if event.key_code == ord("u"):
+                if self._selected_item is not None:
+                    self._context.raise_order(self._selected_item)
+                return None
+            elif event.key_code == ord("d"):
+                if self._selected_item is not None:
+                    self._context.lower_order(self._selected_item)
+                return None
+
+        return super().process_event(event)
+
+    def _update_list(self):
+        options = []
+
+        for i, item in enumerate(self._list):
+            if isinstance(item, FavoriteThread):
+                options.append((item.title, i))
+            else:
+                options.append((item.name, i))
+
+        self._list_box.options = options
+
+    def _on_changed(self):
+        idx = self._list_box.value
+
+        if idx is not None:
+            self._selected_item = self._list[idx]
+
+    def _on_select(self):
+        if self._selected_item is not None:
+            if isinstance(self._selected_item, FavoriteThread):
+                self._context.open_thread(self._selected_item)
+                raise NextScene("Thread")
+            else:
+                self._context.open_board(self._selected_item)
+                raise NextScene("Board")
+
+    def _context_changed(self, e: PropertyChangedEventArgs):
+        self._list = self._context.list
+        self._selected_item = None
+        self._update_list()
