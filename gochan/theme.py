e3f55b289b537c60f31b06a67a882661da667e96
anon <29348502+anon774@users.noreply.github.com>
2020-09-15 08:46:22 +0900

    refactor(theme): use A_NORMAL for attribute instead of A_BOLD

--- gochan/theme.py
+++ gochan/theme.py
@@ -7,31 +7,31 @@ from gochan.widgets.richtext import Brush
 from gochan.widgets.responses_viewer import ThreadBrushes
 
 THEME = {
-    "background": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLACK),
+    "background": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLACK),
     "shadow": (Screen.COLOUR_BLACK, None, Screen.COLOUR_BLACK),
-    "disabled": (Screen.COLOUR_BLUE, Screen.A_BOLD, Screen.COLOUR_BLACK),
-    "invalid": (Screen.COLOUR_YELLOW, Screen.A_BOLD, Screen.COLOUR_RED),
-    "label": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLACK),
-    "borders": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLACK),
-    "scroll": (Screen.COLOUR_CYAN, Screen.A_BOLD, Screen.COLOUR_BLUE),
-    "title": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLUE),
-    "edit_text": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLACK),
-    "focus_edit_text": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLACK),
-    "button": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLACK),
-    "focus_button": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_CYAN),
+    "disabled": (Screen.COLOUR_BLUE, Screen.A_NORMAL, Screen.COLOUR_BLACK),
+    "invalid": (Screen.COLOUR_YELLOW, Screen.A_NORMAL, Screen.COLOUR_RED),
+    "label": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLACK),
+    "borders": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLACK),
+    "scroll": (Screen.COLOUR_CYAN, Screen.A_NORMAL, Screen.COLOUR_BLUE),
+    "title": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLUE),
+    "edit_text": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLACK),
+    "focus_edit_text": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLACK),
+    "button": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLACK),
+    "focus_button": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_CYAN),
     "control": (Screen.COLOUR_YELLOW, Screen.A_NORMAL, Screen.COLOUR_BLUE),
-    "selected_control": (Screen.COLOUR_YELLOW, Screen.A_BOLD, Screen.COLOUR_BLUE),
-    "focus_control": (Screen.COLOUR_YELLOW, Screen.A_BOLD, Screen.COLOUR_BLUE),
-    "selected_focus_control": (Screen.COLOUR_YELLOW, Screen.A_BOLD, Screen.COLOUR_CYAN),
-    "field": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLACK),
-    "selected_field": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLUE),
-    "focus_field": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLACK),
-    "selected_focus_field": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_CYAN),
+    "selected_control": (Screen.COLOUR_YELLOW, Screen.A_NORMAL, Screen.COLOUR_BLUE),
+    "focus_control": (Screen.COLOUR_YELLOW, Screen.A_NORMAL, Screen.COLOUR_BLUE),
+    "selected_focus_control": (Screen.COLOUR_YELLOW, Screen.A_NORMAL, Screen.COLOUR_CYAN),
+    "field": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLACK),
+    "selected_field": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLUE),
+    "focus_field": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLACK),
+    "selected_focus_field": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_CYAN),
 }
 
-THREAD_BRUSHES = ThreadBrushes(Brush(Screen.COLOUR_WHITE, Screen.COLOUR_BLACK, Screen.A_BOLD),
-                               Brush(Screen.COLOUR_GREEN, Screen.COLOUR_BLACK, Screen.A_BOLD),
-                               Brush(Screen.COLOUR_BLUE, Screen.COLOUR_BLACK, Screen.A_BOLD),
+THREAD_BRUSHES = ThreadBrushes(Brush(Screen.COLOUR_WHITE, Screen.COLOUR_BLACK, Screen.A_NORMAL),
+                               Brush(Screen.COLOUR_GREEN, Screen.COLOUR_BLACK, Screen.A_NORMAL),
+                               Brush(Screen.COLOUR_BLUE, Screen.COLOUR_BLACK, Screen.A_NORMAL),
                                Brush(Screen.COLOUR_BLUE, Screen.COLOUR_BLACK, Screen.A_NORMAL),
                                Brush(Screen.COLOUR_RED, Screen.COLOUR_BLACK, Screen.A_NORMAL),
                                )
@@ -66,72 +66,72 @@ if THEME_PATH.is_file():
     if "ui" in theme:
         if "background" in theme["ui"]:
             fg, bg = theme["ui"]["background"]
-            THEME["background"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["background"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "shadow" in theme["ui"]:
             fg, bg = theme["ui"]["shadow"]
-            THEME["shadow"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["shadow"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "disabled" in theme["ui"]:
             fg, bg = theme["ui"]["disabled"]
-            THEME["disabled"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["disabled"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "label" in theme["ui"]:
             fg, bg = theme["ui"]["label"]
-            THEME["label"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["label"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "invalid" in theme["ui"]:
             fg, bg = theme["ui"]["invalid"]
-            THEME["invalid"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["invalid"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "borders" in theme["ui"]:
             fg, bg = theme["ui"]["borders"]
-            THEME["borders"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["borders"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "scroll" in theme["ui"]:
             fg, bg = theme["ui"]["scroll"]
-            THEME["scroll"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["scroll"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "title" in theme["ui"]:
             fg, bg = theme["ui"]["title"]
-            THEME["title"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["title"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "edit_text" in theme["ui"]:
             fg, bg = theme["ui"]["edit_text"]
-            THEME["edit_text"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["edit_text"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "focus_edit_text" in theme["ui"]:
             fg, bg = theme["ui"]["focus_edit_text"]
-            THEME["focus_edit_text"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["focus_edit_text"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "button" in theme["ui"]:
             fg, bg = theme["ui"]["button"]
-            THEME["button"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["button"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "focus_button" in theme["ui"]:
             fg, bg = theme["ui"]["focus_button"]
-            THEME["focus_button"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["focus_button"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "controll" in theme["ui"]:
             fg, bg = theme["ui"]["controll"]
-            THEME["controll"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["controll"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "selected_controll" in theme["ui"]:
             fg, bg = theme["ui"]["selected_controll"]
-            THEME["selected_controll"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["selected_controll"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "focus_controll" in theme["ui"]:
             fg, bg = theme["ui"]["focus_controll"]
-            THEME["focus_controll"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["focus_controll"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "selected_focus_controll" in theme["ui"]:
             fg, bg = theme["ui"]["selected_focus_controll"]
-            THEME["selected_focus_controll"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["selected_focus_controll"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "field" in theme["ui"]:
             fg, bg = theme["ui"]["field"]
-            THEME["field"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["field"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "selected_field" in theme["ui"]:
             fg, bg = theme["ui"]["selected_field"]
-            THEME["selected_field"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["selected_field"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "focus_field" in theme["ui"]:
             fg, bg = theme["ui"]["focus_field"]
-            THEME["focus_field"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["focus_field"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
         elif "selected_focus_field" in theme["ui"]:
             fg, bg = theme["ui"]["selected_focus_field"]
-            THEME["selected_focus_field"] = (to_intcolor(fg), Screen.A_BOLD, to_intcolor(bg))
+            THEME["selected_focus_field"] = (to_intcolor(fg), Screen.A_NORMAL, to_intcolor(bg))
 
     if "thread" in theme:
         if "normal" in theme["thread"]:
             fg, bg = theme["thread"]["normal"]
-            THREAD_BRUSHES.normal = Brush(to_intcolor(fg), to_intcolor(bg), Screen.A_BOLD)
+            THREAD_BRUSHES.normal = Brush(to_intcolor(fg), to_intcolor(bg), Screen.A_NORMAL)
         elif "name" in theme["thread"]:
             fg, bg = theme["thread"]["name"]
-            THREAD_BRUSHES.name = Brush(to_intcolor(fg), to_intcolor(bg), Screen.A_BOLD)
+            THREAD_BRUSHES.name = Brush(to_intcolor(fg), to_intcolor(bg), Screen.A_NORMAL)
         elif "bookmark" in theme["thread"]:
             fg, bg = theme["thread"]["bookmark"]
-            THREAD_BRUSHES.bookmark = Brush(to_intcolor(fg), to_intcolor(bg), Screen.A_BOLD)
+            THREAD_BRUSHES.bookmark = Brush(to_intcolor(fg), to_intcolor(bg), Screen.A_NORMAL)
